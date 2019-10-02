9d442e4fe1a70210db67729df480c51f8022601c
Jonathan Ho <johnnyho82@gmail.com>
2019-10-02 09:54:36 -0400

    Correct PORT bug

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
