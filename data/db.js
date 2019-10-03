636476ecb627f49db1b46ab2895693eb37b86579
Jonathan Ho <johnnyho82@gmail.com>
2019-10-03 12:46:40 -0400

    Restore db.js

--- data/db.js
+++ data/db.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile.js')[environment];
+// const environment = process.env.NODE_ENV || 'development';
+// const config = require('../knexfile.js')[environment];
+
+const config = require('../knexfile.js').development;
 
 module.exports = knex(config);
